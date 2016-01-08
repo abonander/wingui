2514ef5b7004e96d2597f31321ccb15f273da8b4
Austin Bonander <austin.bonander@gmail.com>
2016-01-08 14:33:58 -0800

    Ignore generated `system_classes.rs`

--- src/ffi/system_classes.rs
+++
@@ -1,6 +0,0 @@
-pub const BUTTON: SystemClass = SystemClass(&[66, 117, 116, 116, 111, 110, 0]);
-pub const COMBO_BOX: SystemClass = SystemClass(&[67, 111, 109, 98, 111, 66, 111, 120, 0]);
-pub const STATIC: SystemClass = SystemClass(&[83, 116, 97, 116, 105, 99, 0]);
-pub const EDIT: SystemClass = SystemClass(&[69, 100, 105, 116, 0]);
-pub const LIST_BOX: SystemClass = SystemClass(&[76, 105, 115, 116, 66, 111, 120, 0]);
-pub const SCROLL_BAR: SystemClass = SystemClass(&[83, 99, 114, 111, 108, 108, 66, 97, 114, 0]);
